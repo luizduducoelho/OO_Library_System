2bf938b2a2971ba4c1bacb940589c855fd0ebb87
Luiz Coelho <lelcoelho@yahoo.com.br>
2018-12-07 15:07:25 -0200

    Fix merge issues in biblioteca.h

--- Biblioteca.h
+++ Biblioteca.h
@@ -1,3 +1,6 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+
 #include "Livro.h"
 #include "Publicacao.h"
 #include "Date.h"
@@ -8,10 +11,12 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 class Biblioteca{
-		std::vector<Usuario> lista_usuarios;
-		std::vector<Publicacao> lista_publicacoes;
-		std::vector<Emprestimo> lista_emprestimos;
+		vector<Usuario> lista_usuarios;
+		vector<Publicacao> lista_publicacoes;
+		vector<Emprestimo> lista_emprestimos;
 	public:
 		Biblioteca();
 		void insere_usuario(Usuario user);
@@ -24,12 +29,14 @@ class Biblioteca{
 		void exclui_item_emprestimo(Emprestimo emp, ItemEmprestimo item_emp);
 		void devolve_item(Emprestimo emp, Livro livro);
 		void devolde_todos(Emprestimo emp);
-		std::vector<Publicacao> pesquisa_publicacao(std::string parte_do_titulo);
-		std::vector<Livro> pesquisa_por_autor(std::string parte_do_autor);
-		std::vector<Usuario> get_lista_usuarios() const;
-		std::vector<Publicacao> get_lista_publicacoes() const;
-		std::vector<Emprestimo> get_lista_emprestimos() const;
+		vector<Publicacao> pesquisa_publicacao(std::string parte_do_titulo);
+		vector<Livro> pesquisa_por_autor(std::string parte_do_autor);
+		vector<Usuario> get_lista_usuarios();
+		vector<Publicacao> get_lista_publicacoes();
+		vector<Emprestimo> get_lista_emprestimos();
 		void gravar_em_arquivo();
 		void ler_do_arquivo();
 		
 };
+
+#endif
