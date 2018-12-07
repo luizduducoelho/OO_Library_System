ebfb7490b558e0f03b6cd542299bab671c024e6e
Vitor Gabriel <vitor.c@outlook.com.br>
2018-12-07 02:59:12 -0200

    Biblioteca

--- Biblioteca.h
+++ Biblioteca.h
@@ -1,6 +1,3 @@
-#ifndef BIBLIOTECA_H
-#define BIBLIOTECA_H
-
 #include "Livro.h"
 #include "Publicacao.h"
 #include "Date.h"
@@ -11,12 +8,10 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 class Biblioteca{
-		vector<Usuario> lista_usuarios;
-		vector<Publicacao> lista_publicacoes;
-		vector<Emprestimo> lista_emprestimos;
+		std::vector<Usuario> lista_usuarios;
+		std::vector<Publicacao> lista_publicacoes;
+		std::vector<Emprestimo> lista_emprestimos;
 	public:
 		Biblioteca();
 		void insere_usuario(Usuario user);
@@ -29,14 +24,12 @@ class Biblioteca{
 		void exclui_item_emprestimo(Emprestimo emp, ItemEmprestimo item_emp);
 		void devolve_item(Emprestimo emp, Livro livro);
 		void devolde_todos(Emprestimo emp);
-		vector<Publicacao> pesquisa_publicacao(std::string parte_do_titulo);
-		vector<Livro> pesquisa_por_autor(std::string parte_do_autor);
-		vector<Usuario> get_lista_usuarios();
-		vector<Publicacao> get_lista_publicacoes();
-		vector<Emprestimo> get_lista_emprestimos();
+		std::vector<Publicacao> pesquisa_publicacao(std::string parte_do_titulo);
+		std::vector<Livro> pesquisa_por_autor(std::string parte_do_autor);
+		std::vector<Usuario> get_lista_usuarios() const;
+		std::vector<Publicacao> get_lista_publicacoes() const;
+		std::vector<Emprestimo> get_lista_emprestimos() const;
 		void gravar_em_arquivo();
 		void ler_do_arquivo();
 		
 };
-
-#endif
