00e3b7721f1c13cb3ebc7d42744d5369aa7a31be
Luiz Coelho <lelcoelho@yahoo.com.br>
2018-12-07 15:11:10 -0200

    Added a const

--- Biblioteca.h
+++ Biblioteca.h
@@ -31,7 +31,7 @@ class Biblioteca{
 		void devolde_todos(Emprestimo emp);
 		vector<Publicacao> pesquisa_publicacao(std::string parte_do_titulo);
 		vector<Livro> pesquisa_por_autor(std::string parte_do_autor);
-		vector<Usuario> get_lista_usuarios();
+		vector<Usuario> get_lista_usuarios() const;
 		vector<Publicacao> get_lista_publicacoes();
 		vector<Emprestimo> get_lista_emprestimos();
 		void gravar_em_arquivo();
