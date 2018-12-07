69159c6b4856ca7b123e695d6f531d3f5b093b1a
Luiz Coelho <lelcoelho@yahoo.com.br>
2018-12-07 02:50:48 -0200

    Adicionado os metodos em interface.h

--- Interface.h
+++ Interface.h
@@ -12,6 +12,21 @@ class Interface{
 		Interface();
 		void menu();
 		void cadastra_novo_usuario();
+		void cadastra_novo_livro();
+		void cadastra_novo_periodico();
+		void cadastra_novo_emprestimo();
+		void inserir_novo_item_emprestimo();
+		void excluir_usuario();
+		void excluir_livro();
+		void excluir_periodico();
+		void excluir_emprestimo();
+		void excluir_item_emprestimo();
+		void devolve_todos();
+		void devolve_livro();
+		void pesquisa_publicacao_por_titulo();
+		void pesquisa_livro_por_autor();
+		void lista_publicacoes();
+		void lista_emprestimos();
 		void lista_usuarios();
 };
 
