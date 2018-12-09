55e240b62957cccca56bb6c6fbd61bd1760d0da2
Luiz Coelho <lelcoelho@yahoo.com.br>
2018-12-09 19:32:16 -0200

    Adicionando itens de emprestimo na interface

--- Biblioteca.h
+++ Biblioteca.h
@@ -34,7 +34,7 @@ class Biblioteca{
 		vector<Publicacao> pesquisa_publicacao(std::string parte_do_titulo);
 		vector<Livro> pesquisa_por_autor(std::string parte_do_autor);
 		vector<Usuario> get_lista_usuarios() const {return lista_usuarios;}
-		//vector<Publicacao*> get_lista_publicacoes() const {return lista_publicacoes;}
+		vector<Publicacao*> get_lista_publicacoes() const {return lista_publicacoes;}
 		vector<Emprestimo> get_lista_emprestimos() const {return lista_emprestimos;}
 		void gravar_em_arquivo();
 		void ler_do_arquivo();
--- Interface.cpp
+++ Interface.cpp
@@ -93,7 +93,7 @@ void Interface::menu(){
 				cadastra_novo_emprestimo();
 				break;
 			case 5:
-				//inserir_novo_item_emprestimo();
+				inserir_novo_item_emprestimo();
 				break;
 			case 15:
 				lista_usuarios();
@@ -212,7 +212,7 @@ void Interface::cadastra_novo_emprestimo(){
 	cin.get() ;
 }
 
-/*void Interface::inserir_novo_item_emprestimo(){
+void Interface::inserir_novo_item_emprestimo(){
 	vector<Emprestimo> lista_emprestimos = biblio.get_lista_emprestimos();
 	vector<Publicacao*> lista_publicacoes = biblio.get_lista_publicacoes();
 	int numero, indice_do_emprestimo, indice_do_livro, codPub;
@@ -249,19 +249,20 @@ void Interface::cadastra_novo_emprestimo(){
 		cin.get();
 		return;
 	}
-	/*Livro * livro = dynamic_cast<Livro*>(lista_publicacoes[indice_do_livro]);
-	if(livro == NULL)
-    {
-        cout<<"Falhou!" << endl;
-        return;
-    }
-	ItemEmprestimo item(*livro);
-	lista_emprestimos[indice_do_emprestimo].adiciona_itememprestimo(item);
-	cout << "Item inserido! " << endl;
+	if (dynamic_cast<Livro*>(lista_publicacoes[indice_do_livro])){
+		Livro * livro = dynamic_cast<Livro*>(lista_publicacoes[indice_do_livro]);
+		Livro livro_obj = *livro;
+		ItemEmprestimo item(livro_obj);
+		biblio.insere_item_emprestimo(lista_emprestimos[indice_do_emprestimo], item);
+		cout << "Item " << livro_obj.get_titulo() << " inserido! " << endl;
+	}
+	else{
+		cout << "Erro ao indexar livro" << endl;
+	}
 	cout << "Aperte enter para retornar ao menu ..." << endl;
-	cin.get() ;*/
+	cin.get() ;
 	
-//}
+}
 
 void Interface::lista_usuarios(){
 	vector<Usuario> lista_de_usuarios;
@@ -299,9 +300,10 @@ void Interface::lista_emprestimos(){
 	for (int i=0; i < lista_de_emprestimos.size(); i++){
 		cout << "Emprestimo numero " << lista_de_emprestimos[i].get_numero() << endl;
 		cout << "Usuario: " << lista_de_emprestimos[i].get_nome_usuario() << endl;
-		vector<ItemEmprestimo> itens = lista_de_emprestimos[i].get_itens();
-		for (int j=0; j < itens.size(); j++){
-			cout << "Item " << itens[j].get_titulo() << endl;
+		vector<ItemEmprestimo> vetor_de_itens = lista_de_emprestimos[i].get_itens();
+		cout << "Emprestimo tem " << vetor_de_itens.size() << " livros" << endl;
+		for (int j=0; j < vetor_de_itens.size(); j++){
+			cout << "Item " << vetor_de_itens[j].get_titulo() << endl;
 		}
 		cout << "Data de devolucao: " << lista_de_emprestimos[i].get_data_devolucao() << endl;
 		cout << endl;
